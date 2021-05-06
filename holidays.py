55e507c80bd144760762e4dd118576e042ed41cf
Will Soula <william.soula@starz.com>
2021-05-06 12:31:43 -0600

    Allow to override the date

--- holidays.py
+++ holidays.py
@@ -7,11 +7,16 @@ import os
 class Holidays(BotPlugin):
     """Get Holidays"""
 
-    @botcmd
-    def holidays(self, msg, args):
-        year = int(datetime.now().strftime('%Y'))-1
-        month = datetime.now().strftime('%m')
-        day = datetime.now().strftime('%d')
+    @arg_botcmd('-y', type=int, dest='year', default=0)
+    @arg_botcmd('-m', type=int, dest='month', default=0)
+    @arg_botcmd('-m', type=int, dest='day', default=0)
+    def holiday(self, msg, year, month, day):
+        if year == 0:
+            year = int(datetime.now().strftime('%Y'))-int(os.getenv('YEAR_OFFSET'))
+        if month == 0:
+            month = datetime.now().strftime('%m')
+        if day == 0:
+            day = datetime.now().strftime('%d')
         holiday_api = holidayapi.v1(os.getenv('API_KEY'))
         holiday_parameters = {
             'country': 'US',
